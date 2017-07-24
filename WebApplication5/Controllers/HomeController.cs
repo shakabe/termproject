a4691b9245a5c7a230b59e15419daa07aa98db80
shakabe <bitf14m037@pucit.edu.pk>
2017-07-24 21:06:13 +0500

    term project

---
+++ WebApplication5/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication5.Models;
+
+namespace WebApplication5.Controllers
+{
+    //[Authorize (Roles = "admin,user")]
+    public class HomeController : Controller
+    {
+        projectEntities4 obj = new projectEntities4();
+
+       [Authorize(Roles="admin,user")]
+        public ActionResult Index()
+        {
+
+
+            return View(obj.firstPages.ToList());
+
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult form()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+
+    }
+}
